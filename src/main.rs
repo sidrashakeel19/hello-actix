d2cc37496fbefb929426badda87968f725c89720
sidrashakeel19 <sidrashakeel19@hotmail.com>
2020-05-09 06:53:19 +0500

    init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+// async fn index() -> impl Responder {
+//     HttpResponse::Ok().body("Hello dfdgdgds!")
+// }
+
+// async fn index2() -> impl Responder {
+//     HttpResponse::Ok().body("Hello world again!")
+// }
+
+use actix_web::get;
+
+//#[get("/hello")]
+async fn index3() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    // HttpServer::new(|| {
+    //     App::new()
+    //         .route("/", web::get().to(index))
+    //         .route("/again", web::get().to(index2))
+    // })
+    // .bind("127.0.0.1:8088")?
+    // .run()
+    // .await
+
+    App::new()
+    .service(index3)
+}
